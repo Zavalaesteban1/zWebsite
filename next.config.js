f13ad224bfd956103f1298474ddf20bc70e4ce1a
Esteban Zavala <zavala_esteban4455@yahoo.com>
2024-10-28 18:51:04 -0500

    Fix GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,11 +2,8 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === 'production' ? '/zWebsite' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/zWebsite/' : '',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  basePath: process.env.NODE_ENV === 'production' ? '/developer-portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/developer-portfolio/' : '',
 }
 
 module.exports = nextConfig
