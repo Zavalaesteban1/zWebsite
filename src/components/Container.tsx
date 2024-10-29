4054b0fb2f5440b71fec2a72ffd6844900a5710f
Esteban Zavala <zavala_esteban4455@yahoo.com>
2024-10-29 04:12:30 -0500

    quick changes

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -121,11 +121,11 @@ export default function Container(props: ContainerProps) {
         {/* change probalby with deployment */}
         <meta
           property="og:url"
-          content={`https://zavalaesteban1.github.io/zWebsite/${router.asPath}`}
+          content={`https://zLove.com/${router.asPath}`}
         />
         <link
           rel="canonical"
-          href={`https://zavalaesteban1.github.io/zWebsite/${router.asPath}`}
+          href={`https://zlove.com/${router.asPath}`}
         />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Zavala" />
